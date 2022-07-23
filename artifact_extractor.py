dc12ab1ace7037d88628bd693ea71fa9c6732257
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-07-23 12:32:39 -0700

    Init working pipeline

---
+++ artifact_extractor.py
@@ -0,0 +1,232 @@
+import cv2
+import pathlib
+import numpy as np
+import json
+import time
+import shutil
+import pytesseract
+import string
+import tempfile
+from fastprogress import fastprogress
+
+# MAGIC NUMBERS
+roi = (1217, 153, 612, 1135)
+
+rois = {
+    "artifact_name" :   (  28,   77,  365,   46),
+    "type" :            (  26,  183,  254,   37),
+    "value" :           (  32,  219,  202,   62),
+    "level" :           (  38,  385,   65,   30),
+    "substats_4" :      (  59,  440,  428,  192),
+    "set_name_4" :      (  26,  629,  504,   53),
+    "substats_3" :      (  59,  440,  428,  145),
+    "set_name_3" :      (  26,  581,  504,   53),
+    "equipped" :        ( 100, 1071,  511,   63)
+}
+
+# Constants
+light_text = ["artifact_name", "level", "type", "value"]
+dark_text = ["equipped", "set_name_3", "set_name_4", "substats_3", "substats_4"]
+
+whitelist = set(string.ascii_letters + string.digits + string.whitespace + ".,+%\':")
+
+
+class Artifact():
+    def __init__(self):
+        self.artifact_name = None
+        self.level = None
+        self.type = None
+        self.value = None
+        self.set_name = None
+        self.substats = None
+        self.equipped = None
+
+        self.file_path = None
+
+    def to_dict(self):
+        return {
+            "artifact_name" : self.artifact_name,
+            "level" : self.level,
+            "type" : self.type,
+            "value" : self.value,
+            "set_name" : self.set_name,
+            "substats" : self.substats,
+            "equipped" : self.equipped
+        }
+
+def video_to_frames(input_file_path, output_dir, verbose = True):
+    """Function to extract frames from input video file
+    and save them as separate frames in an output directory.
+    Args:
+        input_file_path: Input video file.
+        output_dir: Output directory to save the frames.
+    Returns:
+        None
+    """
+    try:
+        pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    except OSError:
+        pass
+
+    # Log the time
+    time_start = time.time()
+    # Start capturing the feed
+    cap = cv2.VideoCapture(str(input_file_path))
+    # Find the number of frames
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
+    if verbose:
+        print ("Number of frames: ", video_length)
+    count = 0
+    if verbose:
+        print ("Converting video..\n")
+    # Start converting the video
+    while cap.isOpened():
+        # Extract the frame
+        ret, frame = cap.read()
+        if not ret:
+            # Continue if bad frame
+            count = count + 1
+
+            continue
+        # Write the results back to output location.
+        if verbose:
+            # print(output_dir / f"{(count+1):0>4d}.jpg")
+            print(f"Reading frame {(count+1):0>4d} / {video_length}")
+        frame = crop_roi(frame, roi)
+        cv2.imwrite(str(output_dir / f"{(count+1):0>4d}.jpg"), frame)
+        count = count + 1
+        # If there are no more frames left
+        if (count > (video_length-1)):
+            # Log the time again
+            time_end = time.time()
+            # Release the feed
+            cap.release()
+            # Print stats
+            if verbose:
+                print ("Done extracting frames.\n%d frames extracted" % count)
+                print ("It took %d seconds for conversion." % (time_end-time_start))
+            break
+
+def crop_roi(image, roi):
+    return image[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+
+def crop_frames(frames_dir, output_dir):
+    # Crop all images
+    frames_dir = pathlib.Path(frames_dir)
+    output_dir = pathlib.Path(output_dir)
+    for img_path in sorted(frames_dir.iterdir()):
+        if img_path.suffix == ".jpg":
+            print(img_path.name)
+            image = cv2.imread(str(img_path))
+            cropped_img = crop_roi(image, roi)
+            cv2.imwrite(str(output_dir / img_path.name), cropped_img)
+
+def remove_duplicate_frames(cropped_frames_dir, output_dir):
+    output_dir = pathlib.Path(output_dir)
+    hashes = set()
+    valid_frames = []
+
+    cropped_frames_dir = pathlib.Path(cropped_frames_dir)
+    for img_path in sorted(cropped_frames_dir.iterdir()):
+        if img_path.suffix == ".jpg":
+            image = cv2.imread(str(img_path))
+            img_hash = cv2.img_hash.pHash(image)
+            img_hash = tuple(img_hash[0])
+
+            if img_hash not in hashes:
+                valid_frames.append(img_path)
+                hashes.add(img_hash)
+                print(f"{img_path.name} : {img_hash}")
+
+    print(f"Found {len(hashes)} artifacts")
+
+    for img_path in valid_frames:
+        shutil.copy(img_path, output_dir / img_path.name)
+
+def get_artifact_components(frames_dir, output_dir):
+    # Crop all images
+    frames_dir = pathlib.Path(frames_dir)
+    output_dir = pathlib.Path(output_dir)
+    for img_path in sorted(frames_dir.iterdir()):
+        if img_path.suffix == ".jpg":
+            print(img_path.name)
+            image = cv2.imread(str(img_path))
+            for key, roi in rois.items():
+                cropped_img = crop_roi(image, roi)
+                img_crop_dir = output_dir / img_path.stem
+                img_crop_dir.mkdir(exist_ok=True, parents=True)
+                cv2.imwrite(str(img_crop_dir / f"{key}.jpg"), cropped_img)
+
+def process_light(file_path):
+    image = cv2.imread(str(file_path), cv2.IMREAD_GRAYSCALE)
+
+    _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
+    pad = 20
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, borderType = cv2.BORDER_CONSTANT, value=255)
+    # Dilating helps with light text
+    kernel = np.ones((2,2), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    cv2.imwrite(str(file_path.parent / f"{file_path.stem}_thresh.jpg"), image)
+    return image
+
+def process_dark(file_path):
+    image = cv2.imread(str(file_path), cv2.IMREAD_GRAYSCALE)
+
+    _, image = cv2.threshold(image, 180, 255, cv2.THRESH_BINARY)
+    pad = 20
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, borderType = cv2.BORDER_CONSTANT, value=255)
+    # kernel = np.ones((2,2), np.uint8)
+    # image = cv2.dilate(image, kernel, iterations=1)
+    cv2.imwrite(str(file_path.parent / f"{file_path.stem}_thresh.jpg"), image)
+    return image
+
+def get_ocr_text(image):
+    # Config: https://stackoverflow.com/questions/44619077/pytesseract-ocr-multiple-config-options
+    config="--psm 6"
+    ocr_text = pytesseract.image_to_string(image, config=config)
+    ocr_text.strip()
+    ocr_text = "".join(char for char in ocr_text if char in whitelist)
+    return ocr_text
+
+def extract_text(ocr_dir):
+    ocr_dir = pathlib.Path(ocr_dir)
+    for artifact_dir in ocr_dir.iterdir():
+        if artifact_dir.is_dir():
+            for file_stem in light_text:
+                processed_image = process_light(artifact_dir / (file_stem + ".jpg"))
+                print(f"{file_stem} : {get_ocr_text(processed_image)}")
+
+            for file_stem in dark_text:
+                processed_image = process_dark(artifact_dir / (file_stem + ".jpg"))
+                print(f"{file_stem} : {get_ocr_text(processed_image)}")
+
+verbose = True
+def main(video_path):
+    video_path = pathlib.Path(video_path)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir = pathlib.Path(temp_dir)
+        if verbose:
+            print(f"Created temporary directory at {temp_dir}")
+
+        # Extract frames from video
+        video_output_dir = temp_dir / "video_output"
+        video_output_dir.mkdir(exist_ok=True, parents=True)
+        video_to_frames(video_path, video_output_dir)
+
+        # # Crop extracted frames
+        # cropped_frames_dir = temp_dir / "cropped_frames"
+        # cropped_frames_dir.mkdir(exist_ok=True, parents=True)
+        # crop_frames(video_output_dir, cropped_frames_dir)
+
+        valid_frames_dir = temp_dir / "valid_frames"
+        valid_frames_dir.mkdir(exist_ok=True, parents=True)
+        remove_duplicate_frames(video_output_dir, valid_frames_dir)
+
+        artifact_dir = temp_dir / "artifacts"
+        artifact_dir.mkdir(exist_ok=True, parents=True)
+        get_artifact_components(valid_frames_dir, artifact_dir)
+
+        extract_text(artifact_dir)
+
+if __name__ == "__main__":
+    main("artifacts.mp4")
